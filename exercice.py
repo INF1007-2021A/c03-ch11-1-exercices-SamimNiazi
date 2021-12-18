93f956a41aec9f44e941ba0d6b75b5a6c00cae05
SamimNiazi <samim.niazi@polymtl.ca>
2021-12-18 10:02:18 -0500

    mmm

--- exercice.py
+++ exercice.py
@@ -11,102 +11,29 @@ from game import *
 
 
 class Weapon:
-	
 	UNARMED_POWER = 20
-
-	def __init__(self, name, power, min_level):
+	def __init__(self, name: str, power, min_level):
 		self.__name = name
 		self.power = power
-		self.min_level = min_level
+		self.minlevel = min_level
 
-	@property
+	@property  #sert a acceder aux variable pcq il est prives
 	def name(self):
 		return self.__name
 
-	@classmethod
+	@classmethod #ca sert a quoi???
 	def make_unarmed(cls):
-		return cls("Unarmed", cls.UNARMED_POWER, 1) #comprend pas cette partie
-
+		return cls("Unarmed", cls.UNARMED_POWER, 1 ) #pk cls????
 
 class Character:
-
-	def __init__(self, name, max_hp, attack, defense, level):
+	def __init__(self, name, max_hp, attack, defense, level): #pas mettre Weapon
 		self.__name = name
 		self.max_hp = max_hp
 		self.attack = attack
 		self.defense = defense
 		self.level = level
-		self.weapon = None
+		self.weapon = None   #erreur ici
 		self.hp = max_hp
-	
-	@property
-	def name(self):
-		return self.__name
 
-	@property
-	def weapon(self):
-		return self.__weapon
-
-	@weapon.setter
-	def weapon(self, val):
-		if val is None:
-			val = Weapon.make_unarmed()
-		if val.min_level > self.level:
-			raise ValueError(Weapon)
-		self.__weapon = val
-
-
-	@property
-	def hp(self):
-		return self.__hp
-
-	@hp.setter
-	def hp(self, val):
-		self.__hp = utils.clamp(val, 0, self.max_hp)
-
-	def compute_damage(self, other):
-		level_factor = (2 * self.level) / 5 + 2
-		weapon_factor = self.weapon.power
-		atk_def_factor = self.attack / other.defense
-		critical = random.random() <= 1/16
-		modifier = (2 if critical else 1) * random.uniform(0.85, 1.0)
-		damage = ((level_factor * weapon_factor * atk_def_factor) / 50 + 2) * modifier
-		return int(round(damage)), critical
-
-
-def deal_damage(attacker, defender):
-	damage, crit = attacker.compute_damage(defender)
-	defender.hp -= damage
-	print(f"{attacker.name} used {attacker.weapon.name}")
-	if crit:
-		print("  Critical hit!")
-	print(f"  {defender.name} took {damage} dmg")
-
-def run_battle(c1, c2):
-	attacker = c1
-	defender = c2
-	turns = 1
-	print(f"{attacker.name} starts a battle with {defender.name}!")
-	while True:
-		deal_damage(attacker, defender)
-		if defender.hp == 0:
-			print(f"{defender.name } is sleeping with the fishes.")
-			break
-		turns += 1
-		attacker, defender = defender, attacker
-	return turns
-
-
-def main():
-	c1 = Character("Mei", 1000, 1500, 700, 700)
-	c2 = Character("David", 2, 1, 1, 6)
-
-	c1.weapon = Weapon("my cock", 100, 69)
-	c2.weapon = Weapon("his ass", 120, 1)
-
-	turns = run_battle(c1, c2)
-	print(f"The battle ended in {turns} turns.")
-
-if __name__ == "__main__":
-	main()
+		
 
